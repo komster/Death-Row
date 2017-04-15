1132f49c43ecba25f208c3c0642bf08f733e976d
komster <komster97@gmail.com>
2017-04-15 18:43:11 +0200

    Camera movment

---
+++ Assets/Script/CS_Camera_Movment.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Camera_Movment : MonoBehaviour {
+
+    public GameObject player;
+    public Camera main;
+
+    private Rigidbody2D rb;
+
+    private float playerX;
+    private float playerY;
+    private float cameraX;
+    private float cameraY;
+
+    private float cameraSpeedY;
+    private float cameraSpeedX;
+
+
+    void Start () {
+        main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        playerX = player.transform.position.x;
+        playerY = player.transform.position.y;
+        cameraX = main.transform.position.x;
+        cameraY = main.transform.position.y;
+
+        if (playerY > cameraY - 2 && playerY < cameraY + 2 )
+        {
+            cameraSpeedY = 0f;
+        }
+
+        if (playerY > cameraY + 2 && playerY < cameraY + 3)
+        {
+            cameraSpeedY = 2f;
+        }
+        if (playerY > cameraY + 3 && playerY < cameraY + 4)
+        {
+            cameraSpeedY = 4f;
+        }
+
+        if (playerY < cameraY - 2 && playerY > cameraY - 3)
+        {
+            cameraSpeedY = -2f;
+        }
+        if (playerY < cameraY - 3 && playerY > cameraY - 4)
+        {
+            cameraSpeedY = -4f;
+        }
+
+
+
+
+        if (playerX > cameraX - 9 && playerX < cameraX + 9)
+        {
+            cameraSpeedX = 0f;
+        }
+
+        if (playerX > cameraX + 9 && playerX < cameraX + 10)
+        {
+            cameraSpeedX = 2f;
+        }
+        if (playerX > cameraX + 10 && playerX < cameraX + 11)
+        {
+            cameraSpeedX = 4f;
+        }
+
+        if (playerX < cameraX - 9 && playerX > cameraX - 10)
+        {
+            cameraSpeedX = -2f;
+        }
+        if (playerX < cameraX - 10 && playerX > cameraX - 11)
+        {
+            cameraSpeedX = -4f;
+        }
+
+        rb.velocity = ((transform.up * cameraSpeedY) + (transform.right * cameraSpeedX));
+
+    }
+}
