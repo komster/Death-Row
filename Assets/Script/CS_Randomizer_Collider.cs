9fdd435d43561e199620aa1f98dc336177034993
MarflowWolfram <kubawol@vp.pl>
2017-04-18 10:28:45 +0200

    Island spawning, 4 Islands spawn at random locations when started

---
+++ Assets/Script/CS_Randomizer_Collider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Randomizer_Collider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Island")
+        {
+            col.gameObject.SendMessage("collided");
+            Destroy(this.gameObject);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+       
+	}
+   
+    
+}
