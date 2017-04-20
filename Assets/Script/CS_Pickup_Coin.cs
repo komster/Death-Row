2c12304ac3b9fa1bc0b3a9382449bdb250c1a81c
MarflowWolfram <kubawol@vp.pl>
2017-04-20 18:06:09 +0200

    coin pickups and score

--- Assets/Script/CS_Pickup_Coin.cs
+++ Assets/Script/CS_Pickup_Coin.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CS_Pickup_Coin : MonoBehaviour {
+    private CS_Gamemanager gameManager;
+    public int coinValue;
 
 	// Use this for initialization
 	void Start () {
-		
+        gameManager = FindObjectOfType<CS_Gamemanager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            gameManager.InitScore(coinValue);
+            Destroy(this.gameObject);
+        }
+        
+        
+    }
 }
