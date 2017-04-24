32f817a95ec0701f26ac712cd342680994880067
M1m1c <ludvigbaumolss@hotmail.se>
2017-04-24 17:27:27 +0200

    Added the main menu

---
+++ Assets/Script/CS_Main_Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CS_Main_Menu : MonoBehaviour
+{
+    public GameObject highScoreHolder;
+    public int mainMenuState = 0;
+
+    private List<GameObject> MainMenuList = new List<GameObject>();
+
+    private void Start()
+    {
+        MainMenuList.Add(GameObject.Find("State0"));
+        MainMenuList.Add(GameObject.Find("State1"));
+        MainMenuStateManager();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ShowHighScore()
+    {
+        mainMenuState = 1;
+        MainMenuStateManager();
+
+    }
+    public void GoBackToMainMenu()
+    {
+        mainMenuState = 0;
+        MainMenuStateManager();
+    }
+
+    private void MainMenuStateManager()
+    {
+        foreach (GameObject obj in MainMenuList)
+        {
+            obj.SetActive(false);
+        }
+        MainMenuList[mainMenuState].SetActive(true);
+
+    }
+}
