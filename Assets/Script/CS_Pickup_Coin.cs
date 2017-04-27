ecd81bf7ed812432deb0955a36123b11cfd3bea7
MarflowWolfram <kubawol@vp.pl>
2017-04-27 22:49:49 +0200

    added music to menu, added coin pickup sound to all the coins

--- Assets/Script/CS_Pickup_Coin.cs
+++ Assets/Script/CS_Pickup_Coin.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CS_Pickup_Coin : MonoBehaviour {
     private CS_Gamemanager gameManager;
     public int coinValue;
-
+    public AudioClip sound;
 	// Use this for initialization
 	void Start () {
         gameManager = FindObjectOfType<CS_Gamemanager>();
@@ -19,6 +19,7 @@ public class CS_Pickup_Coin : MonoBehaviour {
     {
         if(other.gameObject.name == "Player")
         {
+            AudioSource.PlayClipAtPoint(sound, transform.position);
             gameManager.InitScore(coinValue);
             Destroy(this.gameObject);
         }
