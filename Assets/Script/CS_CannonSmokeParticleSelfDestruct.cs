fc9ea2ef2121d09345f533c38ebd9b4c8dd4a2e4
M1m1c <ludvigbaumolss@hotmail.se>
2017-05-09 20:46:36 +0200

    Added cannon smoke particle self destruction

---
+++ Assets/Script/CS_CannonSmokeParticleSelfDestruct.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_CannonSmokeParticleSelfDestruct : MonoBehaviour {
+    private ParticleSystem ps;
+
+
+    void Start()
+    {
+        ps = this.gameObject.GetComponentInChildren<ParticleSystem>();
+    }
+
+    void FixedUpdate()
+    {
+        if (ps == true)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
