e4255b6450fe99c50524ae033757120653386e08
M1m1c <ludvigbaumolss@hotmail.se>
2017-05-10 19:05:31 +0200

    Added wave animation and prefabed it

---
+++ Assets/Script/CS_WaveAnimationScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_WaveAnimationScript : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        anim.Play("Anim_WaveAnimation", 0, Random.Range(0.0f, 5.0f));
+    }
+}
