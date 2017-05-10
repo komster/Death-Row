0e5ff96727a8151730654166b4b5c440ef55eee5
MarflowWolfram <kubawol@vp.pl>
2017-05-10 20:59:38 +0200

    Score lose on hit done, spawn pointon being hit is almost done(some bugs left)

--- Assets/Script/CS_Gamemanager.cs
+++ Assets/Script/CS_Gamemanager.cs
@@ -10,27 +10,46 @@ public class CS_Gamemanager : MonoBehaviour {
     public Text score;
     public CS_Gamemanager scoreCoins;
     private CS_Stages stage;
-    
+    public GameObject coinPre;
+    public CS_Player checkIfHit;
+
     private CS_Player_Movment Movement;
     private movment movement;
     private CS_Pickup_Coin pickS;
-    
+
+    public int lostPoints;
     void Start () {
         Movement = GameObject.FindGameObjectWithTag("Player").GetComponent<CS_Player_Movment>();
         movement = GameObject.FindGameObjectWithTag("Player").GetComponent<movment>();
-        
+        checkIfHit = GameObject.FindGameObjectWithTag("Player").GetComponent<CS_Player>();
         stage = this.gameObject.GetComponent<CS_Stages>();
         
         scoreCoins = GameObject.Find("GameManager").GetComponent<CS_Gamemanager>();
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+	    if(checkIfHit.gotHit ==true)
+        {
+            decreScore();
+            checkIfHit.gotHit = false;
+        }
 	}
-
+    public void decreScore()
+    {
+        coins -= 200;
+        score.text = "score: " + coins;
+        Instantiate(coinPre,checkIfHit.playerpos + new Vector3(Random.Range(-10.0f, 20.0f),0, Random.Range(-10.0f, 20.0f)), Quaternion.identity);
+        Instantiate(coinPre, checkIfHit.playerpos + new Vector3(Random.Range(-10.0f, 20.0f), 0, Random.Range(-10.0f, 20.0f)), Quaternion.identity);
+        Instantiate(coinPre, checkIfHit.playerpos + new Vector3(Random.Range(-10.0f, 20.0f), 0, Random.Range(-10.0f, 20.0f)), Quaternion.identity);
+        Instantiate(coinPre, checkIfHit.playerpos + new Vector3(Random.Range(-10.0f, 20.0f), 0, Random.Range(-10.0f, 20.0f)), Quaternion.identity);
+        Instantiate(coinPre, checkIfHit.playerpos + new Vector3(Random.Range(-10.0f, 20.0f), 0, Random.Range(-10.0f, 20.0f)), Quaternion.identity);
+        Instantiate(coinPre, checkIfHit.playerpos + new Vector3(Random.Range(-10.0f, 20.0f), 0, Random.Range(-10.0f, 20.0f)), Quaternion.identity);
+    }
     public void InitScore(int nrOfCoins)
     {
+        
         coins += nrOfCoins;
         score.text = "score: " + coins;
         
@@ -44,11 +63,11 @@ public class CS_Gamemanager : MonoBehaviour {
     private IEnumerator boost()
     {
 
-        Movement.movSpeed += 8f;
-        movement.MovSpeed += 8f;
+        Movement.movSpeed += 4f;
+        movement.MovSpeed += 4f;
         yield return new WaitForSeconds(3f);
-        Movement.movSpeed -= 8f;
-        movement.MovSpeed -= 8f;
+        Movement.movSpeed -= 4f;
+        movement.MovSpeed -= 4f;
         Debug.Log("Boosted");
         
 
