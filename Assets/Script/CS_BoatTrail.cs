c5da178252fe94f46b8c5ad27244d151f59f39e3
M1m1c <ludvigbaumolss@hotmail.se>
2017-05-16 16:37:28 +0200

    Added boat trail and damage smoke prefabs

---
+++ Assets/Script/CS_BoatTrail.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_BoatTrail : MonoBehaviour {
+    public TrailRenderer trail;
+    // Use this for initialization
+    void Start()
+    {
+        trail = this.GetComponent<TrailRenderer>();
+        trail.sortingLayerName = "Background";
+        trail.sortingOrder = -1;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
