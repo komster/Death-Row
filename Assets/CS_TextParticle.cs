5a3924e432e83cc40278f67a5f117e969b496e77
M1m1c <ludvigbaumolss@hotmail.se>
2017-05-16 22:19:46 +0200

    Added The text particle material maker to the scene

---
+++ Assets/CS_TextParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CS_TextParticle : MonoBehaviour {
+
+    public Text scorePickUpText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        scorePickUpText = this.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void ChangeTextOfScorePickUp(string neoText)
+    {
+        scorePickUpText.text = neoText;
+    }
+
+}
