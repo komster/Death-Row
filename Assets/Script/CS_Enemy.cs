f5ae480687fd0e19fb363076958e2ef9f264aa95
komster <komster97@gmail.com>
2017-05-25 19:24:54 +0200

    enemy death animation

--- Assets/Script/CS_Enemy.cs
+++ Assets/Script/CS_Enemy.cs
@@ -24,40 +24,53 @@ public class CS_Enemy : MonoBehaviour {
 
     private float shotDelay = 3;
 
+    private Animator deathAnimation;
+
+    private float deathTimer = 0.6f;
+
     void Start () {
         player = GameObject.Find("Player").GetComponent<Transform>();
         gM = GameObject.Find("GameManager").GetComponent<CS_Gamemanager>();
+        deathAnimation = GetComponent<Animator>();
     }
 	
 	void Update () {
-        float distans = Vector3.Distance(transform.position, player.transform.position);
 
-        float angle = CS_Utils.PointToDegree(player.position - this.transform.position);
-        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
-        
-        if (distans <= 20)
+        if (deathAnimation.enabled == true)
         {
-            
-            shotDelay -= Time.deltaTime;
-            //spotted.Play();
-            if (shotDelay <= 0)
+            deathTimer -= Time.deltaTime;
+            if (deathTimer <= 0)
             {
-                for (int index = 0; index < cannons.Length; index++)
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            float distans = Vector3.Distance(transform.position, player.transform.position);
+
+            float angle = CS_Utils.PointToDegree(player.position - this.transform.position);
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+
+            if (distans <= 20)
+            {
+
+                shotDelay -= Time.deltaTime;
+                //spotted.Play();
+                if (shotDelay <= 0)
                 {
-                    cannonBall.transform.position = cannons[index].position;
-                    GameObject temp = Instantiate(cannonBall);
-                    Rigidbody2D rb = temp.GetComponent<Rigidbody2D>();
-                    rb.velocity = (transform.right * cannonSpeed);
-                    shotDelay = 3;
-                    Instantiate(cannonSmoke, cannons[index].transform.position, Quaternion.LookRotation(rightCannonAimPositions[index].transform.position - cannons[index].transform.position));
+                    for (int index = 0; index < cannons.Length; index++)
+                    {
+                        cannonBall.transform.position = cannons[index].position;
+                        GameObject temp = Instantiate(cannonBall);
+                        Rigidbody2D rb = temp.GetComponent<Rigidbody2D>();
+                        rb.velocity = (transform.right * cannonSpeed);
+                        shotDelay = 3;
+                        Instantiate(cannonSmoke, cannons[index].transform.position, Quaternion.LookRotation(rightCannonAimPositions[index].transform.position - cannons[index].transform.position));
+                    }
                 }
             }
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-       
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -89,7 +102,11 @@ public class CS_Enemy : MonoBehaviour {
 
                 gM.InitScore(300);
                 Instantiate(scoreParticle, this.transform.position, Quaternion.Euler(-90, 0, 0));
-                Destroy(this.gameObject);
+                for (int indez = 0; indez < cannons.Length; indez++)
+                {
+                    Destroy(cannons[indez].gameObject);
+                }
+                deathAnimation.enabled = true;
             }
         }
     }
